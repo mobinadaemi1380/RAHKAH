2fc69c9fb4cfdcb137f33d0c936d6c8fc3b8649b
mobinadaemi1380 <99687107+mobinadaemi1380@users.noreply.github.com>
2022-12-11 08:21:51 +0330

    Add files via upload

---
+++ haltamrinJS 1/js1.js
@@ -0,0 +1,156 @@
+// tamrin 1
+// for (let index = 0; index < 7000; index+=2) {
+//     console.log(index);
+    
+// }
+
+
+//tamrin 2
+// for (let index = 1; index<=10000; index+=2) {
+//     console.log(index);
+    
+// }
+
+
+// //tamrin 3
+// let numbersArray=[1,13,22,123,49]
+// let sum =0
+// for (let index = 0; index < numbersArray.length; index++) {
+    
+//     sum+=numbersArray[index]
+// }
+// console.log(sum);
+
+
+
+// //tamrin 4
+// let numbersArray=[1,13,22,123,49]
+
+// for (let index = 0; index <= numbersArray.length; index++) {
+//     numbersArray = numbersArray.slice(index,index+1)
+    
+// }
+// console.log(numbersArray);
+
+
+
+//tamrin 5
+
+
+// for (let index =1; index <= 8; index++) {
+//     let ada=""
+//     for (let j = 1; j <= index; j++) {
+//         ada+= j+" "
+        
+//     }
+//     console.log(ada);
+    
+
+// }
+
+
+
+//tamrin 6
+// let numbersArray=[1,13,22,123,49]
+// let Max= 0
+    
+// for (let index = 0; index < numbersArray.length; index++) {
+//     let num= numbersArray[index]
+//     if (num>Max) {
+//         Max= num
+//     }
+    
+
+// }
+// console.log(Max);
+
+
+
+//tamrin 7
+
+// let numbersArray=[1,13,22,123,49,17]
+// let Min =numbersArray[0]
+// for (let index = 1; index < numbersArray.length; index++) {
+//     if (Min>numbersArray[index]) {
+//        Min=numbersArray[index]
+        
+//     }
+  
+// }
+// console.log(Min);
+
+
+//tamrin 8
+
+// let numbersArray=[16,13,22,123,49]
+// let Min =numbersArray[0]
+// for (let index = 1; index < numbersArray.length; index++) {
+//     if (Min>numbersArray[index]) {
+//        Min=numbersArray[index]
+        
+//     }
+  
+// }
+// let mimNum=numbersArray.indexOf(Min);
+
+// numbersArray.pop(mimNum)
+// Min =numbersArray[0]
+// for (let index = 1; index < numbersArray.length; index++) {
+//     if (Min>numbersArray[index]) {
+//        Min=numbersArray[index]
+        
+//     }
+  
+// }
+// console.log(Min);
+
+
+
+
+    
+    
+
+
+
+
+//tamrin 9
+
+// let userName=prompt("intre string")
+// let leangth_str=userName.length
+// let reverse=""
+// for ( let index=leangth_str-1; index>=0;index--) {
+//     reverse += userName[index] 
+    
+// }
+// console.log(reverse);
+
+
+
+//tamrin 10
+// for (let index = 1; index < 10; index++) {
+//     for (let l = 1; l < 10; l++) {
+//         console.log(l*index);
+        
+//     }
+    
+// }
+
+
+
+
+
+
+
+//tamrin 11
+// let numbersArray=[1,13,22,13,49,17]
+// for (let index = 0; index < numbersArray.length; index++) {
+//     for (let j =index +1; j < numbersArray.length; j++) {
+//        if (numbersArray[j]== numbersArray[index] ) {
+//           console.log(numbersArray[index]);
+        
+//        }
+//     }
+    
+// }
+
+
